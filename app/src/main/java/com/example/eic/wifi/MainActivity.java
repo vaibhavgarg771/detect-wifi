6f2ad6be217a57cd9c83770bd6a732a69ffa0dff
Vaibhav Garg <vaibhav.garg771@gmail.com>
2017-03-08 01:26:23 +0000

    Initial commit

---
+++ app/src/main/java/com/example/eic/wifi/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.eic.wifi;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    TextView mainText;
+    WifiManager mainWifi;
+    WifiReceiver receiverWifi;
+    List<ScanResult> wifiList;
+    StringBuilder sb = new StringBuilder();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mainText = (TextView) findViewById(R.id.textView);
+        mainWifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        if (mainWifi.isWifiEnabled() == false)
+        {
+            // If wifi disabled then enable it
+            Toast.makeText(getApplicationContext(), "wifi is disabled..making it enabled",
+                    Toast.LENGTH_LONG).show();
+
+            mainWifi.setWifiEnabled(true);
+        }
+        receiverWifi = new WifiReceiver();
+        registerReceiver(receiverWifi, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        mainWifi.startScan();
+        mainText.setText("Starting Scan...");
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0, 0, 0, "Refresh");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+  protected void onPause() {
+        unregisterReceiver(receiverWifi);
+        super.onPause();
+    }
+
+    protected void onResume() {
+        registerReceiver(receiverWifi, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        super.onResume();
+    }
+
+    // Broadcast receiver class called its receive method
+    // when number of wifi connections changed
+
+    class WifiReceiver extends BroadcastReceiver {
+
+        // This method call when number of wifi connections changed
+        public void onReceive(Context c, Intent intent) {
+
+            sb = new StringBuilder();
+            wifiList = mainWifi.getScanResults();
+            sb.append("\n        Number Of Wifi connections :"+wifiList.size()+"\n\n");
+
+            for(int i = 0; i < wifiList.size(); i++){
+
+                sb.append(new Integer(i+1).toString() + ". ");
+                sb.append((wifiList.get(i)).toString());
+                sb.append("\n\n");
+            }
+
+            mainText.setText(sb);
+        }
+
+    }
+
+}
